e68b7c3bc86e856161bf9f8aa2445f720a2a30b8
LordzSpectron <48569093+LordzSpectron@users.noreply.github.com>
2022-05-06 21:25:43 -0300

    update

--- index.js
+++ index.js
@@ -1,44 +1,59 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
 const app = express();
-const fs = require('fs');
 
-app.get('/info/:id', (req, res) => {
-    var idYout = req.params.id;
-    console.log("Coletando informações de: " + idYout)
-    ytdl('https://www.youtube.com/watch?v=' + idYout)
-        .on('info', (info) => {
-            app.set('json spaces', 2)
-            let formats_info = Object.entries(info.formats).reduce((a, [key, value]) => {
-                mimeType = value.mimeType
-                if (mimeType.includes("video")) {
-                    type = "video"
-                    quality = value.qualityLabel
-                } else {
-                    type = "áudio"
-                    quality = value.audioQuality
-                }
-                itag = value.itag
-                fps = value.fps
-                extension = value.container
-                length = bytesToSize(value.contentLength)
-                video = value.hasVideo
-                audio = value.hasAudio
-                url = value.url
-                a.formats_info[key] = { type, itag, quality, length, fps, extension, video, audio, url }
-                return a;
-            }, { formats_info: {} })
-            res.json({
-                id: idYout,
-                name: info.videoDetails.title,
-                length: toTime(info.videoDetails.lengthSeconds),
-                channel: info.videoDetails.author.name,
-                formats: formats_info
-            })
-        });
+app.set('json spaces', 2)
+
+app.get('/', (req, res) => {
+    res.json({
+        status: 'error',
+        reason: 'Youtube id not found'
+    })
+})
+
+app.get('/favicon.ico', (req, res) => {
+    res.sendStatus(404)
 });
 
-app.listen(3000, () => {
+app.get('/:id', (req, res) => {
+    var idYout = req.params.id;
+    console.log("Coletando informações de: " + idYout)
+    try {
+        ytdl('https://www.youtube.com/watch?v=' + idYout)
+            .on('info', (info) => {
+                let formats_info = Object.entries(info.formats).reduce((a, [key, value]) => {
+                    mimeType = value.mimeType
+                    if (mimeType.includes("video")) {
+                        type = "video"
+                        quality = value.qualityLabel
+                    } else {
+                        type = "audio"
+                        quality = value.audioQuality
+                    }
+                    itag = value.itag
+                    fps = value.fps
+                    extension = value.container
+                    length = bytesToSize(value.contentLength)
+                    video = value.hasVideo
+                    audio = value.hasAudio
+                    url = value.url
+                    a.formats_info[key] = { type, itag, quality, length, fps, extension, video, audio, url }
+                    return a;
+                }, { formats_info: {} })
+                res.json({
+                    id: idYout,
+                    name: info.videoDetails.title,
+                    length: toTime(info.videoDetails.lengthSeconds),
+                    channel: info.videoDetails.author.name,
+                    formats: formats_info
+                })
+            });
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+app.listen(process.env.PORT || 3000, () => {
     console.log('Utilizando a porta 3000');
 });
 
